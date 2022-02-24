1c393df27d38a9ceec127523757763c5ba75e686
Sunghwaa <sunghwaa.lee@gmail.com>
2022-02-24 23:54:37 +0900

    feat(router): update block API to handle exception

--- back/src/routers/blocks.ts
+++ back/src/routers/blocks.ts
@@ -20,24 +20,24 @@ router.get('/:blockNumber', async function(req: Request, res: Response, next: Ne
       return res.send(block);
     }
 
-    const blockInfo = await ethereum.getBlock(blockNumber);
-    if(!blockInfo){
-      return next(new BadRequest('잘못된 블럭 번호입니다.'));
-    }
-
-    const blockInDb = MoonbeamBlockEntity.create({
-      number: blockInfo.number,
-      hash: blockInfo.hash,
-      miner: blockInfo.miner,
-      extraData: blockInfo.extraData,
-      gasLimit: blockInfo.gasLimit,
-      gasUsed: blockInfo.gasUsed,
-      baseFeePerGas: blockInfo.baseFeePerGas,
-      transactions: blockInfo.transactions
-    });
-    await blockInDb.save();
-
-    res.send(blockInDb);
+    ethereum.getBlock(blockNumber)
+      .then(blockInfo => {
+        const blockInDb = MoonbeamBlockEntity.create({
+          number: blockInfo.number,
+          hash: blockInfo.hash,
+          miner: blockInfo.miner,
+          extraData: blockInfo.extraData,
+          gasLimit: blockInfo.gasLimit,
+          gasUsed: blockInfo.gasUsed,
+          baseFeePerGas: blockInfo.baseFeePerGas,
+          transactions: blockInfo.transactions
+        });
+        blockInDb.save();
+        res.send(blockInDb);
+      })
+      .catch(err =>{
+        return next(new BadRequest('잘못된 블럭 번호입니다.'));
+      });
   } catch(err) {
     console.log(err);
     throw new Error(err);
