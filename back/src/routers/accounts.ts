3595bc2cc02bc4666b88f7d8b3e388bd84fdc5eb
Jsunmin <tjsals7825@gmail.com>
2022-02-24 21:15:30 +0900

    계정정보 api 추가

--- back/src/routers/accounts.ts
+++ back/src/routers/accounts.ts
@@ -1,8 +1,30 @@
 import express, { NextFunction, Request, Response } from 'express';
+import {AccountEntity} from '../entities';
 import {ethereum} from '../utils/web3';
 import {BadRequest} from '../utils/httpError';
 
 const router = express.Router();
+
+// 특정 계정 정보 겟
+router.get('/:accountAddress', async function(req: Request, res: Response, next: NextFunction) {
+  const {accountAddress} = req.params;
+  if (!accountAddress) {
+    return next(new BadRequest('계정주소가 없습니다.'))
+  }
+
+  try {
+    const account = await AccountEntity.findOne({account: accountAddress});
+    if (!account) {
+      return next(new BadRequest('잘못된 계정주소 입니다.'))
+    }
+    account.balance = await ethereum.getBalance(accountAddress);
+
+    res.send({ account });
+  } catch(err) {
+    console.error(err);
+    return next(err);
+  }
+});
 
 // 계정 잔액 확인
 router.get('/:accountAddress/balance', async function(req: Request, res: Response, next: NextFunction) {
