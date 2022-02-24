a5e5feaf83f4790fe48189f4fcf733ef2ad2b370
Sunghwaa <sunghwaa.lee@gmail.com>
2022-02-24 22:48:57 +0900

    Add block/transaction APIs

--- back/src/entities/transaction.entity.ts
+++ back/src/entities/transaction.entity.ts
@@ -8,6 +8,21 @@ export class MoonbeamTransactionEntity extends BaseEntity implements MoonbeamTra
 
   @Column('int', {nullable: true, comment: '트랜잭션 위치 블록 번호'})
   blockNumber: number | null;
+  
+  @Column('varchar', {comment: '트랜잭션 해시'})
+  hash: string;
+  
+  @Column('varchar', {comment: '발신 주소'})
+  from: string;
+  
+  @Column('varchar', {comment: '수신 주소'})
+  to: string | null;
+  
+  @Column('varchar', {comment: '송금액'})
+  value: string;
+  
+  @Column('varchar', {comment:'가스 가격'})
+  gasPrice: string;
 
   @CreateDateColumn()
   createdAt: Date;
--- back/src/routers/blocks.ts
+++ back/src/routers/blocks.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import { NextFunction, Request, Response } from 'express';
+import { getManager } from 'typeorm';
+import { MoonbeamBlockEntity } from '../entities';
+import { BadRequest } from '../utils/httpError';
 import {ethereum, web3} from '../utils/web3';
 
 const router = express.Router();
@@ -7,9 +10,35 @@ const router = express.Router();
 // 특정 블록 정보 데이터 겟
 router.get('/:blockNumber', async function(req: Request, res: Response, next: NextFunction) {
   const {blockNumber} = req.params;
+  if(!blockNumber){
+    return next(new BadRequest('블럭 번호가 없습니다.'));
+  }
+
   try {
-    console.log(blockNumber)
-    res.send({ block: 'TODO' });
+    const block = await MoonbeamBlockEntity.findOne({number: +blockNumber});
+    if(block){
+      res.send(block);
+    }
+
+    await getManager().transaction(async entityManager => {
+      const blockInfo = await ethereum.getBlock(blockNumber);
+      if(!blockInfo){
+        return next(new BadRequest('잘못된 블럭 번호입니다.'));
+      }
+
+      const block = MoonbeamBlockEntity.create({
+        number: blockInfo.number,
+        hash: blockInfo.hash,
+        miner: blockInfo.miner,
+        extraData: blockInfo.extraData,
+        gasLimit: blockInfo.gasLimit,
+        gasUsed: blockInfo.gasUsed,
+        baseFeePerGas: blockInfo.baseFeePerGas
+      });
+      await entityManager.save(MoonbeamBlockEntity, block);
+
+      res.send(block);
+    });
   } catch(err) {
     console.log(err);
     throw new Error(err);
