0ea47a5faadcdeca91a5688f0f7356d1874910ab
Jsunmin <tjsals7825@gmail.com>
2022-02-25 20:04:54 +0900

    feat(explorer):  api add

--- back/src/routers/explorer.ts
+++ back/src/routers/explorer.ts
@@ -7,8 +7,26 @@ import { ethereum } from '../utils/web3';
 
 const router = express.Router();
 
+// 네트워크 기본 상태 정보
+router.get('/network', async function(req: Request, res: Response, next: NextFunction) {
+  try {
+    const chainId = await ethereum.getChainId();
+    const node = await ethereum.getNodeInfo();
+  
+    const blockNumber = await ethereum.getBlockNumber();
+
+    const protocalVersion = await ethereum.getProtocolVersion();
+  
+
+    res.send({ chainId, node, blockNumber, protocalVersion });
+  } catch(err) {
+    console.log(err);
+    throw new Error(err);
+  }
+});
+
 // 최신 트랜잭션 20개 보여주기
-router.get('transaction/latest', async function(req: Request, res: Response, next: NextFunction) {
+router.get('transactions/latest', async function(req: Request, res: Response, next: NextFunction) {
   try {
 
   } catch(err) {
@@ -18,9 +36,26 @@ router.get('transaction/latest', async function(req: Request, res: Response, nex
 });
 
 // 특정 트랜잭션 정보 겟
-router.get('transaction/:transactionId', async function(req: Request, res: Response, next: NextFunction) {
+router.get('transactions/:transactionId', async function(req: Request, res: Response, next: NextFunction) {
   try {
 
+  } catch(err) {
+    console.log(err);
+    throw new Error(err);
+  }
+});
+
+// 특정 트랜잭션 영수증 정보 겟
+router.get('transactions/:transactionId/recept', async function(req: Request, res: Response, next: NextFunction) {
+  const {transactionId} = req.params;
+  if(!transactionId){
+    return next(new BadRequest('트랜잭션 아이디가 없습니다.'));
+  }
+
+  try {
+    const recept = await ethereum.getTransactionReceipt(transactionId);
+
+    return res.send({ recept });
   } catch(err) {
     console.log(err);
     throw new Error(err);
